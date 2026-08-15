9d86a7e7f9e28c7828a540626a62f0a749e8635a
t545830589 <158136947+t545830589@users.noreply.github.com>
2026-08-15 22:42:25 +0800

    Update theme.dart

--- lib/theme.dart
+++ lib/theme.dart
@@ -1,1 +1,35 @@
 
+import 'package:flutter/material.dart';
+
+
+
+class TianTheme{
+
+
+static BoxDecoration panel={
+
+  color:const Color(0xff071522),
+
+
+  borderRadius:
+
+  BorderRadius.circular(15),
+
+
+
+  border:
+
+  Border.all(
+
+    color:Colors.blueAccent,
+
+    width:1,
+
+  ),
+
+
+};
+
+
+
+}
