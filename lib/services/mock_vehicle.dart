32438a91e907b9824a148f7cf74353f4ffba27cd
t545830589 <158136947+t545830589@users.noreply.github.com>
2026-08-15 22:14:16 +0800

    Update mock_vehicle.dart

--- lib/services/mock_vehicle.dart
+++ lib/services/mock_vehicle.dart
@@ -1,1 +1,85 @@
 
+class MockVehicle {
+
+
+  int battery = 86;
+
+
+  bool doorOpen = false;
+
+
+  bool locked = true;
+
+
+  double temperature = 26.0;
+
+
+
+  String getBattery(){
+
+    return "$battery%";
+
+  }
+
+
+
+  String getDoorStatus(){
+
+    return doorOpen ? "开启" : "关闭";
+
+  }
+
+
+
+  String getLockStatus(){
+
+    return locked ? "已锁" : "未锁";
+
+  }
+
+
+
+  String getTemperature(){
+
+    return "$temperature℃";
+
+  }
+
+
+
+
+  void openDoor(){
+
+    doorOpen = true;
+
+  }
+
+
+
+
+  void closeDoor(){
+
+    doorOpen = false;
+
+  }
+
+
+
+
+  void lockCar(){
+
+    locked = true;
+
+  }
+
+
+
+
+  void unlockCar(){
+
+    locked = false;
+
+  }
+
+
+}
