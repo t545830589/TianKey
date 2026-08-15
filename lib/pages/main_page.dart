3abb980d1454c29261211a3491107bffd481394a
t545830589 <158136947+t545830589@users.noreply.github.com>
2026-08-15 22:40:59 +0800

    Update main_page.dart

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,1 +1,143 @@
+import 'package:flutter/material.dart';
 
+import 'home_page.dart';
+import 'permission_page.dart';
+import 'settings_page.dart';
+
+
+class MainPage extends StatefulWidget{
+
+
+  const MainPage({super.key});
+
+
+  @override
+  State<MainPage> createState()=>_MainPageState();
+
+
+}
+
+
+
+
+class _MainPageState extends State<MainPage>{
+
+
+  int index=0;
+
+
+
+  final pages=[
+
+
+    const HomePage(),
+
+
+    const PermissionPage(),
+
+
+    const SettingsPage(),
+
+
+
+  ];
+
+
+
+
+
+  @override
+  Widget build(BuildContext context){
+
+
+    return Scaffold(
+
+
+
+      body:pages[index],
+
+
+
+
+      bottomNavigationBar:BottomNavigationBar(
+
+
+        currentIndex:index,
+
+
+        backgroundColor:Colors.black,
+
+
+        selectedItemColor:Colors.blueAccent,
+
+
+        unselectedItemColor:Colors.grey,
+
+
+
+        onTap:(i){
+
+
+          setState((){
+
+
+            index=i;
+
+
+          });
+
+
+        },
+
+
+
+        items:[
+
+
+
+          const BottomNavigationBarItem(
+
+            icon:Icon(Icons.home),
+
+            label:"首页",
+
+          ),
+
+
+
+
+          const BottomNavigationBarItem(
+
+            icon:Icon(Icons.people),
+
+            label:"权限",
+
+          ),
+
+
+
+
+          const BottomNavigationBarItem(
+
+            icon:Icon(Icons.settings),
+
+            label:"设置",
+
+          ),
+
+
+
+        ],
+
+
+
+      ),
+
+
+    );
+
+
+  }
+
+
+}
