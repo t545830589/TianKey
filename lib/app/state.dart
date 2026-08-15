9c463a0c0b6388e5b85557aba6187197ee7e7a2c
t545830589 <158136947+t545830589@users.noreply.github.com>
2026-08-15 22:39:48 +0800

    Update state.dart

--- lib/app/state.dart
+++ lib/app/state.dart
@@ -1,1 +1,37 @@
 
+import 'package:flutter/material.dart';
+
+
+class AppState extends ChangeNotifier{
+
+
+  bool connected=false;
+
+
+  String role="无";
+
+
+  String vehicle="陕A0P92Y";
+
+
+  void setConnected(bool value){
+
+    connected=value;
+
+    notifyListeners();
+
+  }
+
+
+
+  void setRole(String value){
+
+    role=value;
+
+    notifyListeners();
+
+  }
+
+
+
+}
