989f86f28a635ddfad4bc03219951afa6b7bac11
t545830589 <158136947+t545830589@users.noreply.github.com>
2026-08-15 23:13:16 +0800

    Update state.dart

--- lib/app/state.dart
+++ lib/app/state.dart
@@ -1,37 +1,102 @@
-
-import 'package:flutter/material.dart';
-
-
-class AppState extends ChangeNotifier{
+import '../services/mock_esp32.dart';
+import '../services/mock_vehicle.dart';
 
 
-  bool connected=false;
+class TianKeyState {
 
 
-  String role="无";
+  final MockESP32 esp32 = MockESP32();
 
 
-  String vehicle="陕A0P92Y";
+  final MockVehicle vehicle = MockVehicle();
 
 
-  void setConnected(bool value){
 
-    connected=value;
+  String get user {
 
-    notifyListeners();
+    return esp32.getCurrentUser();
 
   }
 
 
 
-  void setRole(String value){
+  String get role {
 
-    role=value;
-
-    notifyListeners();
+    return esp32.sessionRole;
 
   }
 
+
+
+  bool get connected {
+
+    return esp32.isConnected();
+
+  }
+
+
+
+  void connect(){
+
+    esp32.connectBLE();
+
+  }
+
+
+
+  void disconnect(){
+
+    esp32.disconnectBLE();
+
+  }
+
+
+
+  String execute(String command){
+
+
+    String result =
+        esp32.executeCommand(command);
+
+
+
+    switch(command){
+
+
+      case "锁车":
+
+        vehicle.lock();
+
+        break;
+
+
+      case "解锁":
+
+        vehicle.unlock();
+
+        break;
+
+
+      case "寻车":
+
+        vehicle.search();
+
+        break;
+
+
+      case "后备箱":
+
+        vehicle.openTrunk();
+
+        break;
+
+
+    }
+
+
+    return result;
+
+  }
 
 
 }
