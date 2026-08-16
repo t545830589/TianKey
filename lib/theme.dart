6ee7b4f4ee334e2dfd8712c78d16e475cff40f19
t545830589 <158136947+t545830589@users.noreply.github.com>
2026-08-16 10:36:11 +0800

    fix: repair invalid TianTheme panel decoration

--- lib/theme.dart
+++ lib/theme.dart
@@ -1,35 +1,14 @@
-
 import 'package:flutter/material.dart';
 
-
-
-class TianTheme{
-
-
-static BoxDecoration panel={
-
-  color:const Color(0xff071522),
-
-
-  borderRadius:
-
-  BorderRadius.circular(15),
-
-
-
-  border:
-
-  Border.all(
-
-    color:Colors.blueAccent,
-
-    width:1,
-
-  ),
-
-
-};
-
-
-
+class TianTheme {
+  static const BoxDecoration panel = BoxDecoration(
+    color: Color(0xff071522),
+    borderRadius: BorderRadius.all(Radius.circular(15)),
+    border: Border.fromBorderSide(
+      BorderSide(
+        color: Colors.blueAccent,
+        width: 1,
+      ),
+    ),
+  );
 }
