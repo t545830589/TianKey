cb861bbb65caeb4001432c3b64b51ec5be9b984c
t545830589 <158136947+t545830589@users.noreply.github.com>
2026-08-16 18:20:02 +0800

    Add ESP32 command protocol layer

---
+++ lib/services/esp32_protocol.dart
@@ -0,0 +1,8 @@
+class Esp32Protocol {
+  static const lock = 'suoche';
+  static const unlock = 'jiesuo';
+  static const find = 'xunche';
+  static const windowUp = 'chuangsheng';
+  static const windowDown = 'chuangjiang';
+  static const trunk = 'houbeixiang';
+}
