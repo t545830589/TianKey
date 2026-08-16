706396abebefc209282fb8ed5c13db6381438f7e
t545830589 <158136947+t545830589@users.noreply.github.com>
2026-08-16 19:03:26 +0800

    Delete lib directory

--- lib/services/esp32_protocol.dart
+++
@@ -1,8 +0,0 @@
-class Esp32Protocol {
-  static const lock = 'suoche';
-  static const unlock = 'jiesuo';
-  static const find = 'xunche';
-  static const windowUp = 'chuangsheng';
-  static const windowDown = 'chuangjiang';
-  static const trunk = 'houbeixiang';
-}
