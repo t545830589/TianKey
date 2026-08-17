bc0916e6cec7c183abde6aee4d53491fd7feb894
t545830589 <158136947+t545830589@users.noreply.github.com>
2026-08-17 22:51:17 +0800

    Fix Esp32Simulator command bridge interface

--- lib/core/esp32/esp32_simulator.dart
+++ lib/core/esp32/esp32_simulator.dart
@@ -14,4 +14,27 @@ class Esp32Simulator {
     status = command.name.toUpperCase();
     return status;
   }
+
+  String executeCommand(String command) {
+    final normalized = command.toUpperCase();
+
+    switch (normalized) {
+      case 'LOCK':
+        return send(Esp32Command.lock);
+      case 'UNLOCK':
+        return send(Esp32Command.unlock);
+      case 'FIND_CAR':
+      case 'FINDCAR':
+        return send(Esp32Command.findCar);
+      case 'WINDOW_UP':
+        return send(Esp32Command.windowUp);
+      case 'WINDOW_DOWN':
+        return send(Esp32Command.windowDown);
+      case 'TRUNK':
+        return send(Esp32Command.trunk);
+      default:
+        status = 'FAILED';
+        return status;
+    }
+  }
 }
