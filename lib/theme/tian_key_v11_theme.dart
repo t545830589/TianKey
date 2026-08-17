3a8382f9390982f142b8e5de1e2cf09266811b68
t545830589 <158136947+t545830589@users.noreply.github.com>
2026-08-17 20:56:28 +0800

    Add V11 visual theme foundation

---
+++ lib/theme/tian_key_v11_theme.dart
@@ -0,0 +1,10 @@
+class TianKeyV11Theme {
+  static const String name = 'TianKey V11';
+
+  static const Map<String, String> effects = {
+    'background': 'tech_dark',
+    'primaryGlow': 'blue',
+    'warningGlow': 'orange',
+    'dangerGlow': 'red',
+  };
+}
