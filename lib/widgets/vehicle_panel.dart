ab3f3b90ecfe0b4b9e835089c52742becf85a044
t545830589 <158136947+t545830589@users.noreply.github.com>
2026-08-18 21:21:30 +0800

    Batch 002-1 improve vehicle panel layout for homepage integration

--- lib/widgets/vehicle_panel.dart
+++ lib/widgets/vehicle_panel.dart
@@ -2,15 +2,33 @@ import 'package:flutter/material.dart';
 
 class VehiclePanel extends StatelessWidget {
   final String imageAsset;
-  const VehiclePanel({super.key, this.imageAsset = 'assets/' });
+  final double height;
+
+  const VehiclePanel({
+    super.key,
+    this.imageAsset = 'assets/home_car_bg.png',
+    this.height = 240,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 220,
+      height: height,
+      width: double.infinity,
+      margin: const EdgeInsets.symmetric(horizontal: 12),
       decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(16),
-        image: DecorationImage(image: AssetImage(imageAsset), fit: BoxFit.cover),
+        borderRadius: BorderRadius.circular(20),
+        boxShadow: const [
+          BoxShadow(
+            blurRadius: 18,
+            spreadRadius: 1,
+            color: Color(0x551595FF),
+          ),
+        ],
+        image: DecorationImage(
+          image: AssetImage(imageAsset),
+          fit: BoxFit.cover,
+        ),
       ),
     );
   }
