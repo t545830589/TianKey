449b3a8d8724a8fb916695120c18c5cfde77b398
t545830589 <158136947+t545830589@users.noreply.github.com>
2026-08-20 18:25:21 +0800

    重构：车辆控制层增加窗口和后备箱控制入口

--- lib/controllers/vehicle_controller.dart
+++ lib/controllers/vehicle_controller.dart
@@ -32,4 +32,19 @@ class VehicleController {
     state.lastCommand = '寻车';
     state.status = '正在执行寻车';
   }
+
+  void raiseWindow() {
+    state.lastCommand = '升窗';
+    state.status = '正在执行升窗';
+  }
+
+  void lowerWindow() {
+    state.lastCommand = '降窗';
+    state.status = '正在执行降窗';
+  }
+
+  void openTrunk() {
+    state.lastCommand = '后备箱';
+    state.status = '正在执行后备箱开启';
+  }
 }
