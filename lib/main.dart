8e9ed48a6f1a7beab297bf81ad0553e276d01e81
t545830589 <158136947+t545830589@users.noreply.github.com>
2026-09-05 01:46:33 +0800

    fix: 移除不必要的 services.dart 导入

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'dart:convert';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
